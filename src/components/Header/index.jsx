3be5376f21c815a64dd83feb71450dbe83ee77cf
Pedro Lucas Fernandes <plff201100@gmail.com>
2024-02-12 14:35:46 -0300

    feat: add search and more content in articles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,41 @@
+import { createSignal } from 'solid-js';
+import { Show } from 'solid-js/web';
 import styles from './Header.module.css';
 import capa from '../../assets/capa-clarim.png';
 import { AiOutlineSearch, AiFillYoutube, AiFillInstagram } from 'solid-icons/ai';
 import { FaBrandsTiktok } from 'solid-icons/fa';
 
 function Header(){
+
+    const [searchOpen, setSearchOpen] = createSignal(false);
+
+    const handleSearchClick = () => {
+        setSearchOpen(prevState => !prevState);
+    };
+
     return (
         <header class={styles.header}>
             <img src={capa} alt="logo"/>
-            <a href="">PÁGINA INICIAL</a>
-            <a href="">NOTÍCIAS</a>
-            <a href="">REVIEWS</a>
-            <a href="">VÍDEOS</a>
+            <nav class={styles.nav}>
+                <a href="">PÁGINA INICIAL</a>
+                <a href="">NOTÍCIAS</a>
+                <a href="">REVIEWS</a>
+                <a href="">VÍDEOS</a>
+            </nav>
 
-            <AiOutlineSearch class={styles.search} size={36}/>
+            <div class={`${styles.searchContainer} ${searchOpen() ? styles.searchOpen : ''}`}>
+                <Show when={searchOpen()}>
+                    <input
+                        type="text"
+                        placeholder="Pesquisar..."
+                        class={styles.searchInput}
+                    />
+                </Show>
+                <AiOutlineSearch class={styles.searchIcon} size={36} onClick={handleSearchClick}/>
+            </div>
+
             <div class={styles.divider}></div>
+
             <AiFillYoutube size={44}/>
             <AiFillInstagram class={styles.icons} size={36}/>
             <FaBrandsTiktok class={styles.icons} size={28}/>
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -9,22 +9,29 @@ function App() {
       <Header />
       
       <main class={styles.main}>
-        <img src={destaque} alt="Notícia destaque" />
+        <div class={styles.destaqueContainer}>
+          <img src={destaque} alt="Notícia destaque" />
+          <div class={styles.theme}>GUIA</div>
+          <h1 class={styles.firstTitle}>THE WITCHER 3: DICAS PARA FINALIZAR O JOGO</h1>
+        </div>
         
         <h2>ÚLTIMAS NOTÍCIAS:</h2>
+        
         <section class={styles.news}>
 
           <div class={styles.noticia1}>
-            <img src={noticia1} alt="Notícia 1" />
+            <div class={styles.imageContainer}>
+              <img src={noticia1} alt="Notícia 1" />
+              <div class={styles.theme}>REVIEW</div>
+            </div>
             <h1>Review: Tekken 8 tem tudo para ser o jogo de luta do ano</h1>
+            <p>Tekken 8 cumpre o que promete com gráficos de nova geração, uma história cinematográfica e modos inovadores para os fãs. Confira a nossa review!</p>
           </div>
 
           <section>
             <div class={styles.noticias}>
               <img src={noticia1} alt="Notícia 1" />
               <h1>Review: Tekken 8 tem tudo para ser o jogo de luta do ano</h1>
-            </div>
-            <div class={styles.noticias}>
               <img src={noticia1} alt="Notícia 1" />
               <h1>Review: Tekken 8 tem tudo para ser o jogo de luta do ano</h1>
             </div>
